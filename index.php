fdcfd0b1a5b50bd18f5eaee65aee7b1b82dbaf55
Darwin Star <darwinstar1@gmail.com>
2021-11-01 23:17:19 +0800

    Add New Css Style

--- index.php
+++ index.php
@@ -5,21 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Portfolio</title>
-    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/index.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css">
 </head>
 <body>
     <div class="container">
 
-    //* Nav Bar
+        <!-- //* Nav Bar -->
         <div class="nav-bar">
+            <i class="fi fi-rr-align-right icon"></i>
+            <h1 class="logo"><a href="">Portfolio</a></h1>
             <ul>
                 <li><a href="#home">Home</a></li>
-                <li><a href="#about">About Me</a></li>
+                <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
             </ul>
         </div>
+        <!-- //* Home Section -->
+        <div class="home">
+            <h1>Welcome to my site</h1>
+        </div>
 
-    
+        <!-- //* About Section -->
+        <div class="about">
+            <p>Hi, My name is Francis Cloyd V. Mag-isa. Welcome to my personal website wherein you will explore the world of mine!</p>
+            <small>This website is all about my personal knowledge and skills that will lead you to understand of mine</small>
+        </div>
+
+        <!-- //*Projects -->
+        <p>This are the projects that i already build:</p>
     </div>
 </body>
 </html>
